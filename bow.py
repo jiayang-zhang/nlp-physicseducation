bc58ac0d5f12f1731c53f4304993b50b2fe61d00
jiayangzhang <niuniu2000@icloud.com>
2022-11-07 23:53:52 +0000

    added comments

--- bow.py
+++ bow.py
@@ -7,27 +7,39 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 # ================================================================================================
-path = '/Users/jiayangzhang/Library/CloudStorage/OneDrive-ImperialCollegeLondon/year4/nlp-physicseducation/testfiles'
-files = ['test01.txt', 'test02.txt']
+path = '/Users/jiayangzhang/Library/CloudStorage/OneDrive-ImperialCollegeLondon/year4/anonymised_reports/year_1_2017/cycle_1/txt'
 # ================================================================================================
 
+'''
+dataframe structure
+index       File        Content         Words...
+    0       GS_x.txt    xyz             (frequency)10
+'''
 # Variables
 corpus_text = []
 corpus_wordvec_names = None
 corpus_wordvec_counts = None
 
 
+
 # create file&token dataframe
 df = pd.DataFrame(columns=['File','Content'])
-# read files
-for file in files:
-    with open(os.path.join(path, file), 'r') as f:
-        content = f.read()
-    content = preprocess(content)
-    # add to corpus_text
-    corpus_text.append(content)
-    # add new row to DataFrame
-    df.loc[len(df)] = [file, content]
+
+counter = 0
+for file in os.listdir(path):
+    if file.startswith('GS_') and file.endswith('.txt'):
+        counter += 1
+        with open(os.path.join(path, file), 'r') as f:
+            content = f.read()
+        content = preprocess(content)
+        # add to corpus_text
+        corpus_text.append(content)
+        # add new row to DataFrame
+        df.loc[len(df)] = [file, content]
+print('Total number of files:', counter)
+
+# print(df)
+
 
 # -- Feature: Bag of Words ---
 countvec = CountVectorizer()
@@ -36,13 +48,13 @@ dtm = countvec.fit_transform( df['Content'].tolist() ) # document-term matrix
 # word vectors
 corpus_wordvec_names = countvec.get_feature_names()
 corpus_wordvec_counts = dtm.toarray()
-print(corpus_wordvec_names)
+# print(corpus_wordvec_names)
 
 # create BoW dataframe
 BoW = pd.DataFrame(dtm.toarray(), columns= countvec.get_feature_names())
 df = pd.merge(df, BoW, left_index=True, right_index=True)
 # # print dataframe (drop content column)
-print(df)
+# print(df)
 
 
 # -- Classification: logistic regression ---
