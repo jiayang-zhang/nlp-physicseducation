092aa31c6f790f0de42823015672e4f0da255c6f
Efia9 <ea119@ic.ac.uk>
2022-11-11 16:57:04 +0000

    naive bayes  method + tfidf - in bow file

--- bow.py
+++ bow.py
@@ -1,44 +1,62 @@
-'''
-performs:
-bag of words
-logistic regression
-'''
-
-from tools.xlsxer import *
-from tools.xmler import *
 import os
 import pandas as pd
-pd.set_option('max_colu', 10)
-from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
+
+# feature extraction imports
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#machine learning method imports
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+# metrics imports
 from sklearn import metrics
-# =======================================================================================================================================
-dir_txtfldr = '/Users/jiayangzhang/Library/CloudStorage/OneDrive-ImperialCollegeLondon/year4/anonymised_reports/year_1_2017/cycle_1/txt'
-# =======================================================================================================================================
-
-# -- Get files ---
-df_files = build_files_dataframe(dir_txtfldr, 'GS_', '.txt')
-
-# -- Get labels ---
-df_labels = build_labels_dataframe('data/labels.xlsx')
-
-# -- Merge dataframes --
-df = pd.merge(df_files, df_labels, left_on='StudentID', right_on='StudentID')      # merged dataframe: StudentID, Content, ArgumentLevel, ReasoningLevel
-print(df.drop(['Content'], axis = 1))
-df.to_csv('data/labels_cleaned.csv')    # save to a csv file
+from sklearn.metrics import classification_report
+# ================================================================================================
+#path = '/Users/jiayangzhang/Library/CloudStorage/OneDrive-ImperialCollegeLondon/year4/nlp-physicseducation/testfiles'
+files = ['test01.txt', 'test02.txt']
+path= '/Users/EfiaA/OneDrive - Imperial College London/Imperial academic work/University life/Y4\MSci project/Project_Coding/nlp-physicseducation/testfiles'
+# ================================================================================================
 
 
 
-# -- Feature extraction: Bag of Words ---
+# create file&token dataframe
+df = pd.DataFrame(columns=['File','Content'])
+# read files
+for file in files:
+    with open(os.path.join(path, file), 'r') as f:
+        content = f.read()
+    # add new row to DataFrame
+    df.loc[len(df)] = [file, content]
+
+
+# -- Feature: Bag of Words ---
 countvec = CountVectorizer()
-dtm = countvec.fit_transform( df['Content'].tolist() )  # document-term matrix
+dtm = countvec.fit_transform( df['Content'].tolist() ) # document-term matrix
+# create BoW dataframe
+BoW = pd.DataFrame(dtm.toarray(), columns= countvec.get_feature_names())
+# join two dataframes
+df = pd.merge(df, BoW, left_index=True, right_index=True)
+# print dataframe (drop content column)
+print(df.drop('Content',axis=1))
 
-# word vectors
-corpus_wordvec_names = countvec.get_feature_names()     # word vector name list
-corpus_wordvec_counts = dtm.toarray()                   # word vector frequncy list    # len(corpus_wordvec_counts) = index(dataframe)
+#%%
+#%%
+# -- Feature: TF-IDF ---
+def tf_idf(dataframe):
+    # performs tf_idf on the dataframe
+    v    = TfidfVectorizer()
+    x    = v.fit_transform(dataframe)
+    #s_m  = x.toarray()
+    return x
 
+print(tf_idf(df['Content']))
+
+#%%
 
 
 # -- Classification: logistic regression ---
@@ -49,6 +67,7 @@ y = np.array([0,1]) #dummy_label
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 # Create an instance of LogisticRegression classifier
 #
+
 lr = LogisticRegression(C=100.0, random_state=1, solver='lbfgs', multi_class='ovr')
 #
 # Fit the model
@@ -62,3 +81,13 @@ y_predict = lr.predict(X_test)
 # Use metrics.accuracy_score to measure the score
 print("LogisticRegression Accuracy %.3f" %metrics.accuracy_score(y_test, y_predict))
 '''
+
+# --- Classification: Naive Bayes classifier ----
+X = dtm.toarray()
+y = df['Reasoning level']
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+clf       = MultinomialNB().fit(X_train, y_train)
+predicted = clf.predict(X_test)
+
+print('MultinomialNB Accuracy:', metrics.accuracy_score(y_test, predicted))
+print(classification_report(y_test, predicted))
