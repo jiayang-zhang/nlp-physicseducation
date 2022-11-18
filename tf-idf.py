2055564fd58b183be26c2c8c99ea1adf77778326
Efia9 <ea119@ic.ac.uk>
2022-11-18 13:56:16 +0000

    added random forest and cross validation to evaluate accuracy

--- tf-idf.py
+++ tf-idf.py
@@ -1,8 +1,6 @@
-
-
+#%%
 import pandas as pd
 import numpy as np
-
 
 # feature extraction imports
 from sklearn.feature_extraction.text import CountVectorizer
@@ -62,22 +60,32 @@ predicted_b = clf_b.predict(X_test_b)
 #%%
 #from sklearn.model_selection import StratifiedKFold
 kf = KFold(n_splits=5)
-#skf = StratifiedKFold(n_splits=10)
+#skf = StratifiedKFold(n_splits=10) --> the results are worse for stratified sampling 
+
+
+
 # -- NB, TF-IDF, KFOLD ----
+array = []
 for train_index, test_index in kf.split(X_t):
-    print('TRAIN:', train_index, 'TEST:', test_index)
+    #print('TRAIN:', train_index, 'TEST:', test_index)
     X_train_t, X_test_t = X_t[train_index], X_t[test_index]
     y_train_t, y_test_t = y[train_index], y[test_index]
     clf_t      = MultinomialNB().fit(X_train_t, y_train_t)
     predicted_t = clf_b.predict(X_test_t)
-    print(predicted_t)
+    #print(predicted_t)
+    acc_score = metrics.accuracy_score(y_test_t, predicted_t)
+    #print('MultinomialNB Accuracy, using TF-IDF feature extraction:', acc_score)
+    array.append(acc_score)
 
+average_accuracy = sum(array)/len(array)]
+print('Array of accuracy values:', array)
+print('Average accuracy:', average_accuracy)
 
 '''
 ----- KEY INFORMATION! --------
 For the train_test_split the documentation shows
  1. random_state = 42 | controls the shuffling applied in data before applying the split#
-  link: https://scikit-learn.org/stable/glossary.html#term-random_state
+link: https://scikit-learn.org/stable/glossary.html#term-random_state
  2. shuffle = True | you can shuffle before splitting 
  3. stratify = 
 
@@ -86,8 +94,11 @@ More on the different functions to use to split shuffle and stratify data
 
  Stratified k fold
  stratified shuffle split
-'''
 
+ best source, used it for tfidf kfold training
+ https://neptune.ai/blog/cross-validation-in-machine-learning-how-to-do-it-right
+'''
+#%%
 print('MultinomialNB Accuracy, using BOW feature extraction:', metrics.accuracy_score(y_test_b, predicted_b))
 print('MultinomialNB Accuracy, using TF-IDF feature extraction:', metrics.accuracy_score(y_test_t, predicted_t))
 #print(classification_report(y_test_, predicted))
