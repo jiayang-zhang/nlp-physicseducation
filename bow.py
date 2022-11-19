e15b4dbef78cbaaa8e9d4703a6827f9c70d8c958
jiayangzhang <niuniu2000@icloud.com>
2022-11-19 12:34:28 +0000

    looping over different settings

--- bow.py
+++ bow.py
@@ -9,42 +9,53 @@ from tools import ml_tools, utils, formats
 # =======================================================================================================================================
 dir_csv = 'outputs/labels_cleaned.csv'
 # =======================================================================================================================================
+# * set how you would like it to be trained *
+labels = ['ArgumentLevel','ReasoningLevel'] # 'ArgumentLevel', 'ReasoningLevel'
+features = 'ifidf' #'bow', 'ifidf'
+num_epochs = 10
+train_sizes = [0.5,0.6,0.7,0.8,0.9] # proportion of training data
+# =======================================================================================================================================
 
 # unpack dataframe
 df = pd.read_csv(dir_csv, encoding='utf-8')
 
-'''
-# -- Feature extraction: Bag of Words ---
-wordvec_names, wordvec_counts = ml_tools.BoW(df['Content'].tolist())
-'''
-
 # -- Feature extraction: TF-IDF ---
-wordvec_names, wordvec_counts = ml_tools.tf_idf(df['Content'].tolist())
+if feature ==  'ifidf':
+    wordvec_names, wordvec_counts = ml_tools.tf_idf(df['Content'].tolist())
+# -- Feature extraction: Bag of Words ---
+elif feature == 'bow':
+    wordvec_names, wordvec_counts = ml_tools.BoW(df['Content'].tolist())
+
+
+
+# loop over labels, feature extractions
+for label in labels:
+    for feature in features:
+        lr_accuracy_trainsize_plot(label, feature, num_epochs, train_sizes)
 
 
 # -- classifion: logistic regression --
-sizes = [0.5,0.6,0.7,0.8,0.9]
-accuracies = []
-num_epochs = 10
+def lr_accuracy_trainsize_plot(label, feature, num_epochs, train_sizes):
+    accuracies = []
+    for size in train_sizes:
+        sum = 0
+        start_time = time.time()
+        for epoch in range(num_epochs):
+            # split train/test data
+            X_train, X_test, y_train, y_test = train_test_split(wordvec_counts, df[label].tolist(), train_size = size)
+            # train model + prediction
+            y_test_predict = ml_tools.logistic_regression(X_train, X_test, y_train)
 
-for size in sizes:
-    sum = 0
-    start_time = time.time()
-    for epoch in range(num_epochs):
-        # split train/test data
-        X_train, X_test, y_train, y_test = train_test_split(wordvec_counts, df['ArgumentLevel'].tolist(), train_size = size)
-        # train model + prediction
-        y_test_predict = ml_tools.logistic_regression(X_train, X_test, y_train)
-
-        # Accuracy
-        accuracy_score = ml_tools.sanity_check(X_test, y_test, y_test_predict, printWrong=False)
-        # print("Epoch {}/{}, Accuracy: {:.3f}".format(epoch+1,num_epochs, accuracy_score))
-        sum += accuracy_score
-    accuracies.append(sum/num_epochs)
-    print('average accuracy score:', sum/num_epochs)
-    print("--- %s seconds ---" % (time.time() - start_time))
+            # Accuracy
+            accuracy_score = ml_tools.sanity_check(X_test, y_test, y_test_predict, printWrong=False)
+            # print("Epoch {}/{}, Accuracy: {:.3f}".format(epoch+1,num_epochs, accuracy_score))
+            sum += accuracy_score
+        accuracies.append(sum/num_epochs)
+        print('average accuracy score:', sum/num_epochs)
+        print("--- %s seconds ---" % (time.time() - start_time))
 
 
-# specify figure output path
-filepath = 'outputs/ifidf-lr-{}epochs.png'.format(num_epochs) # ** always change name **
-formats.scatter_plot(xvalue = sizes, yvalue = accuracies, xlabel = 'Training Size', ylabel = 'Accuracy', filepath)
+    # specify figure output path
+    filepath = 'outputs/{}-lr-{}epochs-{}.png'.format(feature, num_epochs, label) # ** always change name **
+    formats.scatter_plot(xvalue = train_sizes, yvalue = accuracies, xlabel = 'Training Size', ylabel = 'Accuracy', filepath = filepath)
+    return
