1ba0bd3278b2d42d7eb93ae9e1049eeb02420bf1
Efia9 <ea119@ic.ac.uk>
2023-02-15 11:46:53 +0000

    changes have been added to this

--- NN_tools.py
+++ NN_tools.py
@@ -39,10 +39,7 @@ def NN_ck(model1, input1, ephs, X_t, y_t, X_test, y_test):
     model.compile(loss= 'binary_crossentropy', optimizer= 'rmsprop', metrics= ['accuracy'])
     model.build(input1)
     model.summary()
-    history = model.fit(X_t,y_t,epochs = ephs, verbose=True, validation_data=(X_test, y_test), batch_size=30 )
-    predictions = model.predict(X_test[:11])
-    cohen_score = cohen_kappa_score(y_test[:11], predictions)
-    return cohen_score
+    return model
 
 def NN_optimised_parameters(model1, input1, ephs, X_t, y_t, X_test, y_test):
     maxlen = 100 
@@ -50,14 +47,14 @@ def NN_optimised_parameters(model1, input1, ephs, X_t, y_t, X_test, y_test):
     model.add(layers.Dense(12, input_dim = input1, activation  = 'relu'))
     model.add(layers.Dense(8, input_dim = input1, activation  = 'relu'))
     model.add(layers.Dense(1,activation = 'sigmoid'))
+    #model.add(layers.Dense(5,activation = 'softmax'))
     model.add(layers.Flatten())
     model.compile(loss= 'binary_crossentropy', optimizer= 'rmsprop', metrics= ['accuracy'])
+    #model.compile(loss= 'sparse_categorical_crossentropy', optimizer= 'rmsprop', metrics= ['accuracy'])
     model.build(input1)
     model.summary()
-    history = model.fit(X_t,y_t,epochs = ephs, verbose=True, validation_data=(X_test, y_test), batch_size=30 )
-    predictions = model.predict(X_test[:11])
-    cohen_score = cohen_kappa_score(y_test[:11], predictions)
-    return history
+    #history = model.fit(X_t,y_t,epochs = ephs, verbose=True, validation_data=(X_test, y_test), batch_size=30 )
+    return model
 
 def NN_default_parameters(model1, input1, ephs, X_t, y_t, X_test, y_test):
     maxlen = 100 
@@ -69,10 +66,8 @@ def NN_default_parameters(model1, input1, ephs, X_t, y_t, X_test, y_test):
     model.compile(loss= 'binary_crossentropy', optimizer= 'Adam', metrics= ['accuracy'])
     model.build(input1)
     model.summary()
-    history = model.fit(X_t,y_t,epochs = ephs, verbose=True, validation_data=(X_test, y_test), batch_size=30 )
-    predictions = model.predict(X_test[:11])
-    cohen_score = cohen_kappa_score(y_test[:11], predictions)
-    return history, cohen_score
+    #history = model.fit(X_t,y_t,epochs = ephs, verbose=True, validation_data=(X_test, y_test), batch_size=30 )
+    return model
 
 
 def NN_data(Neural, X, y,t_size,epoch_no, str_dataname,str_featext, str_year, dir):
@@ -90,11 +85,18 @@ def NN_data(Neural, X, y,t_size,epoch_no, str_dataname,str_featext, str_year, di
         X_train_b, X_test_b, y_train_b, y_test_b = train_test_split(X, y , train_size = i)
         input = X_train_b.shape[1]
         #input = len(y_train_b)
-        nn1 = Neural(Sequential(), input, epoch_no, X_train_b, y_train_b, X_test_b, y_test_b)
+        model = Neural(Sequential(), input, epoch_no, X_train_b, y_train_b, X_test_b, y_test_b)
+        history = model.fit(X_train_b,y_train_b,epochs = epoch_no, verbose=True, validation_data=(X_test_b, y_test_b), batch_size=30 )
+        predictions =  model.predict(X_test_b) 
+        predictions =  np.argmax(predictions, axis=1)
+        y_test_b    =  np.argmax(y_test_b, axis=1)
+        cohen_score = cohen_kappa_score(y_test_b, predictions)
 
-        dummy.append(nn1.history['accuracy'])
-        dummy_loss.append(nn1.history['loss'])
-        dummy_val_loss.append(nn1.history['loss'])
+        dummy.append(cohen_score)
+        dummy.append(history.history['accuracy'])
+        dummy_loss.append(history.history['loss'])
+        dummy_val_loss.append(history.history['loss'])
+        dummy_cohen.append(cohen_kappa_score)
 
 
     for array in dummy:
@@ -107,7 +109,9 @@ def NN_data(Neural, X, y,t_size,epoch_no, str_dataname,str_featext, str_year, di
     for array in dummy_val_loss:
         val_loss.append(np.sum(array)/len(array))
     
+    for array in dummy_cohen:
         ck.append(np.sum(dummy_cohen)/len(dummy_cohen))
+    
 
     dict_rl = {'trainsize':t_size, 'accuracy':accuracies, 'sem': accuracies_sem, 'loss':loss, 'valloss': val_loss, 'ck':ck}
     acc_rl_bow = pd.DataFrame(dict_rl)
@@ -136,52 +140,85 @@ def NN_data_iteration(Neural, X, y,t_size,epoch_no, it_no, str_dataname,str_feat
         dummy_loss     = []
         dummy_val_loss = []
         dummy_ck       = []
+        dummy_predict  = []
 
         for iteration in range(it_no):
             X_train_b, X_test_b, y_train_b, y_test_b = train_test_split(X, y , train_size = i)
             input = X_train_b.shape[1]
-            nn1 = Neural(Sequential(), input, epoch_no, X_train_b, y_train_b, X_test_b, y_test_b)
+            model = Neural(Sequential(), input, epoch_no, X_train_b, y_train_b, X_test_b, y_test_b)
+            history = model.fit(X_train_b,y_train_b,epochs = epoch_no, verbose=True, validation_data=(X_test_b, y_test_b), batch_size=30 )
+            predictions =  model.predict(X_test_b) 
+            predictions =  np.argmax(predictions, axis=1)
+            y_test_b    =  np.argmax(y_test_b, axis=1)
+            print('prediction', predictions)
+            print('y_test_b', y_test_b)
 
-            dummy.append(nn1.history['accuracy'])
-            dummy_loss.append(nn1.history['loss'])
-            dummy_val_loss.append(nn1.history['loss'])
+            #cohen_score = cohen_kappa_score(y_test_b, predictions)
 
-        
+            # len(x) for x in lst
+            #dummy_ck.append(cohen_score)
+            dummy_predict.append(predictions)
+            dummy.append(history.history['accuracy'])
+            dummy_loss.append(history.history['loss'])
+            dummy_val_loss.append(history.history['loss'])
+            #dummy_ck.append(cohen_kappa_score)
+
+        print(dummy_ck)
         for no in range(len(dummy)):
             accuracies1.append(np.sum(dummy[no])/len(dummy[no]))
             accuracies_sem1.append(np.sum(sem(dummy[no]))/len(dummy[no]))
             loss1.append(np.sum(dummy_loss[no])/len(dummy[no]))
             val_loss1.append(np.sum(dummy_val_loss[no])/len(dummy[no]))
 
+
+                
     
 
         accuracies.append(np.sum(accuracies1)/len(accuracies1))
         accuracies_sem.append(np.sum(accuracies_sem1)/len(accuracies_sem1))
         loss.append(np.sum(loss1)/len(loss1))
         val_loss.append(np.sum(val_loss1)/len(val_loss1))
-        ck.append(np.sum(dummy_ck)/len(dummy_ck))
 
-    dict_rl = {'trainsize':t_size, 'accuracy':accuracies, 'sem': accuracies_sem, 'loss':loss, 'valloss': val_loss, 'ck':ck}
+        # if dummy_ck == 0.0:
+        #     ck.append(dummy_ck)
+        # else:
+        #     ck.append(np.sum(dummy_ck))
+        
+        #print(ck)
+
+
+    #dict_rl = {'trainsize':t_size, 'accuracy':accuracies, 'sem': accuracies_sem, 'loss':loss, 'valloss': val_loss, 'ck':ck}
+    dict_rl = {'trainsize':t_size, 'accuracy':accuracies, 'sem': accuracies_sem, 'loss':loss, 'valloss': val_loss }
     acc_rl_bow = pd.DataFrame(dict_rl)
-    name = 'W5_NN_{}_iteration_{}_{}_{}_{}'. format(it_no, str_year, str_featext,str_dataname,epoch_no)
-    utils.save_as_pickle_file(acc_rl_bow,name, dir)
+    #name = 'W5_NN_{}_iteration_{}_{}_{}_{}'. format(it_no, str_year, str_featext,str_dataname,epoch_no)
+    #utils.save_as_pickle_file(acc_rl_bow,name, dir)
     return acc_rl_bow
 
 def NN_data_ck(Neural, X, y,t_size,epoch_no, str_year):
     ck    = []
     dummy = []
+
     for i in t_size:
         X_train_b, X_test_b, y_train_b, y_test_b = train_test_split(X, y , train_size = i)
         input = X_train_b.shape[1]
         nn1 = Neural(Sequential(), input, epoch_no, X_train_b, y_train_b, X_test_b, y_test_b)
-
-        dummy.append(nn1)
+        history = nn1.fit(X_train_b,y_train_b,epochs = epoch_no, verbose=True, validation_data=(X_test_b, y_test_b), batch_size=30 )
+        predictions =  nn1.predict(X_test_b) 
+        predictions =  np.argmax(predictions, axis=1)
+        y_test_b    =  np.argmax(y_test_b, axis=1)
+        cohen_score = cohen_kappa_score(y_test_b, predictions)
+        dummy.append(cohen_score)
+    
+    for array in dummy:
+        ck.append(np.sum(array)/epoch_no)
 
     dict_rl = {'trainsize':t_size, 'ck':ck}
     acc_rl_bow = pd.DataFrame(dict_rl)
-    name = 'W5_NN_cohenkappa_{}_{}ephs_recent'. format(str_year, epoch_no)
-    utils.save_as_pickle_file(acc_rl_bow,name, dir)
+    #name = 'W5_NN_cohenkappa_{}_{}ephs_recent'. format(str_year, epoch_no)
+    #utils.save_as_pickle_file(acc_rl_bow,name, dir)
     return acc_rl_bow
+
+
 
 def plot(dframe_train, dframe_acc, dframe_sem):
     plt.plot(dframe_train, dframe_acc, 'o')
@@ -227,26 +264,41 @@ def nn_graph(train_size, df, mlmodel):
     for i in range(4):
         # all of the arrays will be of size 4 for training size graphs anyway
         plt.plot(train_size,df['accuracy'][i], label = '{}-{}-{}'.format(mlmodel,df['feature extraction'][i],df['Label'][i]), color =c[i],marker='o', markersize =4 )
-        #plt.scatter(train_size, df['accuracy'][i], 'o', color = c[i])
         plt.errorbar(train_size, df['accuracy'][i], yerr = df['sem'][i], linewidth=0.2, capsize=2, c = 'black', markeredgewidth = 0.5)
     plt.xlabel(' Training size')
-    plt.ylabel('Accuracy score')
+    plt.ylabel('Average Accuracy score')
     plt.legend( prop={'size': 3})
     #filepath = 'outputs/comparison-{}-2500epochs-overallgraph_new.png'.format(mlmodel)
     #plt.savefig(filepath)
     return 
 
 def nn_graph_loss(train_size, df, mlmodel):
-    #train_sizes = [0.5,0.6,0.7,0.8,0.9]
+
     c = ['r','b','g', 'm'] # colour notation
+
     for i in range(4):
         # all of the arrays will be of size 4 for training size graphs anyway
         plt.plot(train_size,df['loss'][i], label = '{}-{}-{}-{}'.format(mlmodel,df['feature extraction'][i],df['Label'][i], 'loss'), color =c[i],marker='o', markersize =4 )
-        #plt.plot(train_size,df['val_loss'][i], label = '{}-{}-{}-{}'.format(mlmodel,df['feature extraction'][i],df['Label'][i], 'val_loss'), color =c[i],marker='o', markersize =4 )
-        #plt.scatter(train_size, df['accuracy'][i], 'o', color = c[i])
-        #plt.errorbar(train_size, df['loss'][i], yerr = df['sem'][i], linewidth=0.2, capsize=2, c = 'black', markeredgewidth = 0.5)
+
+
     plt.xlabel(' Training size')
-    plt.ylabel('Loss')
+    plt.ylabel('Average Loss')
+    plt.legend( prop={'size': 3})
+    #filepath = 'outputs/comparison-{}-2500epochs-overallgraph_new.png'.format(mlmodel)
+    #plt.savefig(filepath)
+    return
+
+def nn_graph_general(train_size, df_col, mlmodel, y_axis_string):
+
+    c = ['r','b','g', 'm'] # colour notation
+
+    for i in range(4):
+        # all of the arrays will be of size 4 for training size graphs anyway
+        plt.plot(train_size,df_col[i], label = '{}-{}-{}-{}'.format(mlmodel,df['feature extraction'][i],df['Label'][i], 'loss'), color =c[i],marker='o', markersize =4 )
+
+
+    plt.xlabel(' Training size')
+    plt.ylabel(y_axis_string)
     plt.legend( prop={'size': 3})
     #filepath = 'outputs/comparison-{}-2500epochs-overallgraph_new.png'.format(mlmodel)
     #plt.savefig(filepath)
